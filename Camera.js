58d65de31ada67e6bdecf279c61b88f19b228951
maticstric <matic@likar.me>
2020-05-28 12:34:56 -0700

    Fixed up camera a bit

--- Camera.js
+++ Camera.js
@@ -3,5 +3,9 @@ class Camera {
     this.eye = new Vector3([0, 0, 8]);
     this.lookAt = new Vector3([0, 0, -100]);
     this.up = new Vector3([0, 1, 0]);
+
+    this.fov = 45;
+    this.near = 0.1;
+    this.far = 100;
   }
 }
