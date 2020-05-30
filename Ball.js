93c339679be136d51e7ebe1c0f530b12fdaf8f75
Eric Sandoval <esandov8@ucsc.edu>
2020-05-30 03:47:25 -0700

    Rewrite ball as child of GameObject and add getters for velocity components

--- Ball.js
+++ Ball.js
@@ -1,33 +1,25 @@
-class Ball {
+class Ball extends GameObject{
+  get velocityX() {return this.velocity.elements[0];}
+  get velocityY() {return this.velocity.elements[1];}
+  get velocityZ() {return this.velocity.elements[2];}
 
   constructor() {
-    this.sphere = new Sphere();
+    super();
+    this.model = new Sphere();
     this.velocity = new Vector3([0, 0, 0]);
 
-    this.sphere.positionMatrix.setTranslate(0, -8.75, 0); // Inital position
-    this.sphere.scaleMatrix.setScale(0.35, 0.35, 0.35);
+    this.translate(0, -8.75, 0); // Inital position
+    this.scale(0.35, 0.35, 0.35);
   }
 
   move() {
     if (g_gameStarted) {
       // Update position based on velocity if the game has started
-      let x = this.velocity.elements[0];
-      let y = this.velocity.elements[1];
-      let z = this.velocity.elements[2];
-
-      this.sphere.positionMatrix.translate(x, y, z);
+      this.translate(velocityX, velocityY, velocityZ);
     } else {
       // Otherwise constrain it to the paddle
-      let position = this.sphere.positionMatrix.elements;
-      position[X] =  g_paddle.position[X];
+      this.positionX =  g_paddle.positionX;
     }
   }
 
-  position() {
-    let x = this.sphere.positionMatrix.elements[12];
-    let y = this.sphere.positionMatrix.elements[13];
-    let z = this.sphere.positionMatrix.elements[14];
-
-    return new Vector3([x, y, z]);
-  }
 }
