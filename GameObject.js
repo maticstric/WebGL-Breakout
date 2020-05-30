b7254fb88208ee86fd07613ccab44cfb1dd81803
Eric Sandoval <esandov8@ucsc.edu>
2020-05-30 02:57:15 -0700

    Create getters and setters for position components

--- GameObject.js
+++ GameObject.js
@@ -1,15 +1,12 @@
-const VECTOR_X = 0;
-const VECTOR_Y = 1;
-const VECTOR_Z = 2;
+// For accessing vectors inside and outside class
+const X = 0;
+const Y = 1;
+const Z = 2;
 
+// For accessing matrices inside class
 const MATRIX_X = 12;
 const MATIRX_Y = 13;
 const MATIRX_Z = 14;
-
-// Temporary
-const X = 12;
-const Y = 13;
-const Z = 14;
 
 class GameObject {
   get position() {
@@ -21,13 +18,37 @@ class GameObject {
   }
 
   set position(v) {
-    let x = this.elements[VECTOR_X];
-    let y = this.elements[VECTOR_Y];
-    let z = this.elements[VECTOR_Z];
+    let x = this.elements[X];
+    let y = this.elements[Y];
+    let z = this.elements[Z];
 
     this.model.positionMatrix.elements[MATRIX_X] = x;
     this.model.positionMatrix.elements[MATIRX_Y] = y;
     this.model.positionMatrix.elements[MATIRX_Z] = z;
+  }
+
+  get positionX() {
+    return this.model.positionMatrix.elements[MATRIX_X];
+  }
+
+  get positionY() {
+    return this.model.positionMatrix.elements[MATRIX_Y];
+  }
+
+  get positionZ() {
+    return this.model.positionMatrix.elements[MATRIX_Z];
+  }
+
+  set positionX(n) {
+    this.model.positionMatrix.elements[MATRIX_X] = n;
+  }
+
+  set positionY(n) {
+    this.model.positionMatrix.elements[MATRIX_Y] = n;
+  }
+
+  set positionZ(n) {
+    this.model.positionMatrix.elements[MATRIX_Z] = n;
   }
 
   constructor() {
